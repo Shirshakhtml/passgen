931e3432b781e9d23e1af3d234a1e14d8ee20886
Shirshak Roy <75179984+Shirshakhtml@users.noreply.github.com>
2023-01-07 12:11:19 +0530

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,6 +1,7 @@
 import random
 import string
 import pyfiglet
+import secrets
 
 def banner(text):
     return pyfiglet.figlet_format(text)
@@ -9,8 +10,8 @@ print(banner('PASSGEN'))
 
 def generate_password():
     length = int(input("Enter the desired password length: ")) # Ask the user for random password length
-    characters = string.ascii_lowercase + string.digits + string.hexdigits + string.ascii_uppercase + string.octdigits # Get a random assortment of numerals, uppercase letters, and lowercase letters
-    password = ''.join(random.sample(characters, length)) # In order to get a random password, mix up the characters
+    characters = string.ascii_lowercase + string.digits + string.hexdigits + string.punctuation + string.ascii_uppercase + string.octdigits # Get a random assortment of numerals, uppercase letters, and lowercase letters
+    password = ''.join(secrets.choice(characters) for i in range(length)) # In order to get a random password, mix up the characters
     return password
 
 print(generate_password())
